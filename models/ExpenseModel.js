834861476594de827f1b4e649061461aa353175b
zaleskiy88 <zaleskiy88@gmail.com>
2024-07-27 19:57:25 +0100

    changed type field  in ExpenseSchema

--- models/ExpenseModel.js
+++ models/ExpenseModel.js
@@ -4,7 +4,7 @@ const ExpenseSchema = new mongoose.Schema(
   {
     title: { type: String, required: true, trim: true, maxLength: 50 },
     amount: { type: Number, required: true, trim: true, maxLength: 20 },
-    type: { type: String, default: "income" },
+    type: { type: String, default: "expense" },
     date: { type: Date, required: true },
     category: { type: String, required: true },
     description: { type: String, required: true },
