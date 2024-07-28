a655ccd768a5a26607827e54448b612a5b55914b
zaleskiy88 <zaleskiy88@gmail.com>
2024-07-28 16:52:07 +0100

    added new field type

--- models/CategoryModel.js
+++ models/CategoryModel.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const CategorySchema = new mongoose.Schema({
   value: { type: String, required: true, trim: true },
   title: { type: String, required: true, trim: true },
+  type: { type: String, required: true, trim: true },
 });
 
 module.exports = mongoose.model("Category", CategorySchema);
