37fca0d7a08142d669e5e4fcc170c962310388c4
zaleskiy88 <zaleskiy88@gmail.com>
2024-07-28 16:52:36 +0100

    minor updates

--- models/ExpenseModel.js
+++ models/ExpenseModel.js
@@ -4,7 +4,7 @@ const ExpenseSchema = new mongoose.Schema(
   {
     title: { type: String, required: true, trim: true, maxLength: 50 },
     amount: { type: Number, required: true, trim: true, maxLength: 20 },
-    type: { type: String, default: "expense" },
+    type: { type: String, default: "expenses" },
     date: { type: Date, required: true },
     category: { type: String, required: true },
     description: { type: String, required: true },
--- models/IncomeModel.js
+++ models/IncomeModel.js
@@ -4,7 +4,7 @@ const IncomeSchema = new mongoose.Schema(
   {
     title: { type: String, required: true, trim: true, maxLength: 50 },
     amount: { type: Number, required: true, trim: true, maxLength: 20 },
-    type: { type: String, default: "income" },
+    type: { type: String, default: "incomes" },
     date: { type: Date, required: true },
     category: { type: String, required: true },
     description: { type: String, required: true },
