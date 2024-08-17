d7ff07721155b8006ed8e3b570fca043e352f2f8
zaleskiy88 <zaleskiy88@gmail.com>
2024-08-17 20:37:49 +0100

    added validateBody() middleware function to validate body requests

--- routes/expenses.js
+++ routes/expenses.js
@@ -1,10 +1,14 @@
 const router = require("express").Router();
 const { expensesControllers: ctrl } = require("../controllers/index");
+const { validateBody } = require("../middlewares/index");
+const { ExpenseSchemas } = require("../models/index");
+//  ===============================================================  //
 
+const { addExpenseSchema } = ExpenseSchemas;
 ///Expense Routes///
 router.get("/get-expenses", ctrl.getExpenses);
-router.post("/add-expense", ctrl.addExpense);
-router.put("/update-expense/:id", ctrl.updateExpense);
+router.post("/add-expense", validateBody(addExpenseSchema), ctrl.addExpense);
+router.put("/update-expense/:id", validateBody(addExpenseSchema), ctrl.updateExpense);
 router.delete("/delete-expense/:id", ctrl.deleteExpense);
 
 module.exports = router;
--- routes/incomes.js
+++ routes/incomes.js
@@ -1,10 +1,14 @@
 const router = require("express").Router();
 const { incomesControllers: ctrl } = require("../controllers/index");
+const { validateBody } = require("../middlewares/index");
+const { IncomeSchemas } = require("../models/index");
+//  ===============================================================  //
 
+const { addIncomeSchema } = IncomeSchemas;
 ///Income Routes///
 router.get("/get-incomes", ctrl.getIncomes);
-router.post("/add-income", ctrl.addIncome);
-router.put("/update-income/:id", ctrl.updateIncome);
+router.post("/add-income", validateBody(addIncomeSchema), ctrl.addIncome);
+router.put("/update-income/:id", validateBody(addIncomeSchema), ctrl.updateIncome);
 router.delete("/delete-income/:id", ctrl.deleteIncome);
 
 module.exports = router;
