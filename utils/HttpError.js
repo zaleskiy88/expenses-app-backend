a568349069ae5063f8ac3428cf6240114c35a73c
zaleskiy88 <zaleskiy88@gmail.com>
2024-08-17 19:04:01 +0100

    added JSDocs

--- utils/HttpError.js
+++ utils/HttpError.js
@@ -1,4 +1,10 @@
 const HttpError = (status, message) => {
+  /**
+   * Creates a new HttpError object with the specified status and message.
+   * @param {number} status - The HTTP status code of the error.
+   * @param {string} message - A description of the error.
+   * @return {Error} A new HttpError object.
+   */
   const error = new Error();
   error.status = status;
   error.message = message;
--- utils/logger.js
+++ utils/logger.js
@@ -2,6 +2,14 @@ const { appendFile } = require("fs/promises");
 const moment = require("moment");
 
 const logger = async (req, res, next) => {
+  /**
+   * Logs the request method, URL, and date/time to a server log file.
+   * @param {Object} req - The HTTP request object.
+   * @param {Object} res - The HTTP response object.
+   * @param {Function} next - The next middleware function.
+   * @return {Promise} A Promise that resolves when the log entry is written to the file.
+   */
+
   const date = moment().format("DD/MM/YYYY, HH:mm:ss");
   await appendFile(
     "./public/server.log",
