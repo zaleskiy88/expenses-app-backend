5c8f713e9a6b6c70f41798580940c33a61c22dd6
unknown <zaleskiy88@gmail.com>
2024-08-24 12:10:16 +0100

    logger in app.js deactivated

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const { connectDB } = require("./db/db");
 const { readdirSync } = require("fs");
 const express = require("express");
 const cors = require("cors");
-const { logger } = require("./middlewares/index");
+const { /*logger,*/ HttpError } = require("./utils/index");
 
 const app = express();
 const PORT = process.env.PORT;
@@ -11,21 +11,20 @@ const PORT = process.env.PORT;
 ///Middlewares///
 app.use(express.json());
 app.use(cors());
-app.use(logger);
+//app.use(logger);
 
 ///Routes///
 readdirSync("./routes").map((route) => {
   app.use("/api/v1", require("./routes/" + route));
 });
 
+///Error handlers Middlewares///
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
 
-///Error catcher middleware///
 app.use((err, req, res, next) => {
-  const { status = 500, message = "Server error" } = err;
-  res.status(status).json({ message });
+  res.status(500).json({ message: err.message });
 });
 
 ///Server///
