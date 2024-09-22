26c5674205fa93bc55903a57a8bce69a7dd1e924
zaleskiy88 <zaleskiy88@gmail.com>
2024-09-22 18:18:14 +0100

    added bcrypt package to the project; added password hashing

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,17 +1,19 @@
 const { UserSchemas } = require("../../models/index");
 const { controllerWrapper, HttpError } = require("../../utils/index");
+const bcrypt = require("bcrypt");
 
 const { User } = UserSchemas;
 
 const register = async (req, res) => {
-  const { name, email, password } = req.body;
+  const { email, password } = req.body;
   const user = await User.findOne({ email });
 
   if (user) {
     throw HttpError(409, "Email already in use");
   }
+  const hashPassword = await bcrypt.hash(password, 10);
 
-  const newUser = User({ name, email, password });
+  const newUser = User({ ...req.body, password: hashPassword });
   await newUser.save();
 
   res.status(201).json({ message: "User created successfully" });
