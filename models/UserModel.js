52be651d1dcb305c91d3d5c9f4deeccf8bc8b1b2
zaleskiy88 <zaleskiy88@gmail.com>
2024-09-22 17:02:11 +0100

    added User model&schemas

---
+++ models/UserModel.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+const { mongooseErrorHandler } = require("../utils/index");
+
+const emailRegexp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/;
+
+//  ================Mongoose Schema===================================  //
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, match: emailRegexp, unique: true },
+    password: { type: String, minength: 6, required: true },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+//Schema error handling
+UserSchema.post("save", mongooseErrorHandler);
+
+const User = mongoose.model("User", UserSchema);
+
+//  ================Joi Schema===================================  //
+
+const RegisterSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+});
+
+const LoginSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+});
+
+module.exports = { User, RegisterSchema, LoginSchema };
--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const Category = require("./CategoryModel");
 const ExpenseSchemas = require("./ExpenseModel");
 const IncomeSchemas = require("./IncomeModel");
+const UserSchemas = require("./UserModel");
 //  ===================================================//
 
-module.exports = { Category, ExpenseSchemas, IncomeSchemas };
+module.exports = { Category, ExpenseSchemas, IncomeSchemas, UserSchemas };
