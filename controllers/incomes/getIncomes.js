6415dd85c25fab1a2202302999046d979c79cf4a
zaleskiy88 <zaleskiy88@gmail.com>
2024-09-29 12:25:55 +0100

    minor changes

--- controllers/incomes/getIncomes.js
+++ controllers/incomes/getIncomes.js
@@ -1,7 +1,9 @@
 const { IncomeSchemas } = require("../../models/index");
 const { HttpError, controllerWrapper } = require("../../utils/index");
 //  ===================================================//
+
 const { Income } = IncomeSchemas;
+
 const getIncomes = async (req, res, next) => {
   const incomes = await Income.find().sort({ createdAt: -1 });
 
