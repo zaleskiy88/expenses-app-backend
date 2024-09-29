ab86494bfe508afb22053306c64fd64440571881
zaleskiy88 <zaleskiy88@gmail.com>
2024-09-29 12:24:07 +0100

    added authenticate middleware

--- routes/incomes.js
+++ routes/incomes.js
@@ -1,14 +1,14 @@
 const router = require("express").Router();
 const { incomesControllers: ctrl } = require("../controllers/index");
-const { validateBody } = require("../middlewares/index");
+const { validateBody, authenticate } = require("../middlewares/index");
 const { IncomeSchemas } = require("../models/index");
 //  ===============================================================  //
 
 const { addIncomeSchema } = IncomeSchemas;
 ///Income Routes///
-router.get("/incomes/get-incomes", ctrl.getIncomes);
-router.post("/incomes/add-income", validateBody(addIncomeSchema), ctrl.addIncome);
-router.put("/incomes/update-income/:id", validateBody(addIncomeSchema), ctrl.updateIncome);
-router.delete("/incomes/delete-income/:id", ctrl.deleteIncome);
+router.get("/incomes/get-incomes", authenticate, ctrl.getIncomes);
+router.post("/incomes/add-income", authenticate, validateBody(addIncomeSchema), ctrl.addIncome);
+router.put("/incomes/update-income/:id", authenticate, validateBody(addIncomeSchema), ctrl.updateIncome);
+router.delete("/incomes/delete-income/:id", authenticate, ctrl.deleteIncome);
 
 module.exports = router;
