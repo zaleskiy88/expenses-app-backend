3f235d5134b9c9c858ab5c93bdec2594a55977a3
zaleskiy88 <zaleskiy88@gmail.com>
2024-09-29 18:09:24 +0100

    added logout logic

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,5 +1,6 @@
 const register = require("./register");
 const login = require("./login");
-const logOut = require("./logOut");
+const logout = require("./logout");
+const getCurrent = require("./current");
 
-module.exports = { register, login, logOut };
+module.exports = { register, login, logout, getCurrent };
--- controllers/auth/logOut.js
+++ controllers/auth/logOut.js
@@ -1,3 +1,19 @@
-const logOut = () => {};
+const { controllerWrapper, HttpError } = require("../../utils/index");
+const { UserSchemas } = require("../../models/index");
 
-module.exports = { logOut };
+//========================================================//
+
+const { User } = UserSchemas;
+
+const logout = async (req, res) => {
+  const { _id } = req.user;
+  const user = await User.findByIdAndUpdate(_id, { token: "" });
+
+  if (!user) {
+    throw HttpError(401, "Not authorized");
+  }
+
+  res.status(200).json({ message: "logout success" });
+};
+
+module.exports = controllerWrapper(logout);
